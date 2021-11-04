d1835f5541a04a4549000577b7b9578161d19a23
Genaro <laureanomg2@gmail.com>
2021-11-04 17:08:22 -0600

    test

--- tests/unit/ToggleButton.spec.js
+++ tests/unit/ToggleButton.spec.js
@@ -1,7 +1,3 @@
-import { shallowMount } from '@vue/test-utils'
-import { expect } from 'chai'
-import ToggleButton from '@/components/ToggleButton.vue'
-
 describe('ToggleButton', () => {
 
     // First Test Case
