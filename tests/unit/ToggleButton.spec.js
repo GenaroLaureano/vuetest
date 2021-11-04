8aa91295b0d8670685299d9051a4b3944d3b4a19
Genaro <laureanomg2@gmail.com>
2021-11-04 16:42:52 -0600

    se agrega un test

---
+++ tests/unit/ToggleButton.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import { expect } from 'chai'
+import ToggleButton from '@/components/ToggleButton.vue'
+
+describe('ToggleButton', () => {
+
+    // First Test Case
+    it ('should render correctly', () => {})
+});
